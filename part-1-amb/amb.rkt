a29a176893ca7a5d7a391c7a0e1b0458ad6bca2e
Karol Marcjan <karol.marcjan@gmail.com>
2016-07-08 10:21:04 +0200

    Reduction relation from amb (verbatim from tutorial - broken)

--- part-1-amb/amb.rkt
+++ part-1-amb/amb.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require redex)
+(require redex/tut-subst)
 
 (define-language L
   [e (e e)
@@ -66,10 +67,10 @@
    (types Γ e_3 t)
    -----------------------------
    (types Γ (if0 e_1 e_2 e_3) t)]
-
+  
   [-------------------
    (types Γ (amb t) t)]
-
+  
   [(types Γ e_h t)
    (types Γ (amb t e_t ...) t)
    -------------------------------
@@ -99,3 +100,58 @@
          t)
   t)
  (term ((→ num num))))
+
+(define-extended-language Ev L+Γ
+  (p (e ...))
+  (P (e ... E e ...))
+  (E (v E)
+     (E e)
+     (+ v ... E e ...)
+     (if0 E e e)
+     (fix E)
+     hole)
+  (v (λ (x t) e)
+     (fix v)
+     number))
+
+(define-metafunction Ev
+  Σ : number ... -> number
+  [(Σ number ...)
+   ,(apply + (term (number ...)))])
+
+(define-metafunction Ev
+  subst : x v e -> e
+  [(subst x v e)
+   ,(subst/proc x? (list (term x)) (list (term v)) (term e))])
+
+(define x? (redex-match Ev x))
+
+(define red
+  (reduction-relation
+   Ev
+   #:domain p
+   
+   (--> (in-hole P (if0 0 e_1 e_2))
+        (in-hole P e_1)
+        "if0t")
+   
+   (--> (in-hole P (if0 v e_1 e_2))
+        (in-hole P e_2)
+        (side-condition (not (equal? 0 (term v))))
+        "if0f")
+   
+   (--> (in-hole P ((fix (λ (x t) e)) v))
+        (in-hole P (((λ (x t) e) (fix (λ (x t) e))) v))
+        "fix")
+   
+   (--> (in-hole P ((λ (x t) e) v))
+        (in-hole P (subst x v e))
+        "βv")
+   
+   (--> (in-hole P (+ number ...))
+        (in-hole P (Σ number ...))
+        "+")
+   
+   (--> (e_1 ... (in-hole E (amb e_2 ...)) e_3 ...)
+        (e_1 ... (in-hole E e_2) ... e_3 ...)
+        "amb")))
